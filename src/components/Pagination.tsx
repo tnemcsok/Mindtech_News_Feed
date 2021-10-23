0939106cd59f8ddb14ffcc0369582ea5c6cd3f2f
Tamas Nemcsok <tamas.nemcsok@gmail.com>
2021-10-24 00:40:48 +0200

    Pagination ready
    Pagination component was created and rendered on posts page. Six post per page is rendered.

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type PaginationPros = { page: number; totalPages: number; setPage: any };
+export const Pagination = ({ page, totalPages, setPage }: PaginationPros) => {
+  return (
+    <nav>
+      <ul className="pagination justify-content-center">
+        <li>
+          <button
+            className={"page-link mx-3"}
+            onClick={() => {
+              setPage(page - 1);
+            }}
+            disabled={page == 1}
+          >
+            Previous
+          </button>
+        </li>
+        <li>
+          <button
+            className={"page-link mx-3"}
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            disabled={page == totalPages}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- src/features/posts/Posts.tsx
+++ src/features/posts/Posts.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Spinner } from "../../components/Spinner";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { selectAllPosts, fetchPosts } from "./postsSlice";
 import { Post } from "../../app/types";
 import { PostCard } from "./PostCard";
+import { Pagination } from "../../components/Pagination";
 
 export const Posts = () => {
   const posts = useAppSelector(selectAllPosts);
@@ -12,18 +13,23 @@ export const Posts = () => {
   const postStatus = useAppSelector((state) => state.posts.status);
   const error = useAppSelector((state) => state.posts.error);
 
+  let [page, setPage] = useState(1);
+
   useEffect(() => {
     if (postStatus === "idle") {
       dispatch(fetchPosts());
     }
   }, [postStatus, dispatch]);
 
+  let totalPages = 1;
   let renderedPosts;
 
   if (postStatus === "loading") {
     renderedPosts = <Spinner text="Loading..." />;
   } else if (postStatus === "succeeded") {
-    renderedPosts = posts.map((post: Post) => {
+    totalPages = Math.ceil(posts.length / 6);
+    const sliced = posts.slice((page - 1) * 6, page * 6);
+    renderedPosts = sliced.map((post: Post) => {
       return <PostCard key={post.id} post={post} />;
     });
   } else {
@@ -33,6 +39,9 @@ export const Posts = () => {
   return (
     <div className="p-5 container">
       <div className="row">{renderedPosts}</div>
+      {postStatus === "succeeded" && (
+        <Pagination page={page} setPage={setPage} totalPages={totalPages} />
+      )}
     </div>
   );
 };
